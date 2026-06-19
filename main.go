5e82d7063bc7f1984b77de8f044874d03e3d4b37
Vibe Maxer <->
2026-06-19 18:51:42 +0200

    Handle -o flags

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	outFile := flag.String("o", "", "output file (default: stdout)")
+	flag.Parse()
+
 	var src []byte
 	var err error
 
-	if len(os.Args) > 1 {
-		src, err = os.ReadFile(os.Args[1])
+	if flag.NArg() > 0 {
+		src, err = os.ReadFile(flag.Arg(0))
 	} else {
 		src, err = io.ReadAll(os.Stdin)
 	}
@@ -23,5 +27,14 @@ func main() {
 	tokens := NewLexer(string(src)).Tokenize()
 	file := NewParser(tokens).Parse()
 	out := NewGenerator().Generate(file)
-	fmt.Print(out)
+
+	if *outFile != "" {
+		err = os.WriteFile(*outFile, []byte(out), 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Print(out)
+	}
 }
